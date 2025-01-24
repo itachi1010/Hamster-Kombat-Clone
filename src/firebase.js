47b38d5d410a542132c2212093dc01e531329370
itachi1010 <onahben977@gmail.com>
2025-01-24 13:08:16 +0100

    pushing my hamster kombat

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,25 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// const firebaseConfig = {
+//   // Replace with your Firebase configuration
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "YOUR_AUTH_DOMAIN",
+//   projectId: "YOUR_PROJECT_ID",
+//   storageBucket: "YOUR_STORAGE_BUCKET",
+//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+//   appId: "YOUR_APP_ID"
+// };
+
 const firebaseConfig = {
-  // Replace with your Firebase configuration
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDm2gq8-thtDKph4FbeMhtCWL2Wa-difLY",
+  authDomain: "blop-54e72.firebaseapp.com",
+  projectId: "blop-54e72",
+  storageBucket: "blop-54e72.appspot.com",
+  messagingSenderId: "279479184441",
+  appId: "1:279479184441:web:6e3fccb28c9c93912c71e8",
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
